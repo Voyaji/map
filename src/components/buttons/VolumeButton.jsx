e982ff303996896921414e1bda7f0e044a5a4079
mercklyzer <mercklyzer@gmail.com>
2023-01-08 21:11:56 +0800

    Make volume svg flex

--- src/components/buttons/VolumeButton.jsx
+++ src/components/buttons/VolumeButton.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 
 const VolumeOnButton = () => {
     return (
-        <svg className='hover:fill-yellow-button group cursor-pointer container'
+        <svg className='hover:fill-yellow-button group cursor-pointer container flex'
             width="80" height="80" viewBox="0 0 80 80" fill="none" xmlns="http://www.w3.org/2000/svg">
             <defs>
                 <style>{`
@@ -44,7 +44,7 @@ const VolumeOnButton = () => {
 
 const VolumeOffButton = () => {
     return (
-        <svg className='hover:fill-yellow-button group cursor-pointer container'
+        <svg className='hover:fill-yellow-button group cursor-pointer container flex'
             width="80" height="80" viewBox="0 0 80 80" fill="none" xmlns="http://www.w3.org/2000/svg">
             <defs>
                 <style>{`
