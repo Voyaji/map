19d8b253bc08c82aaec39e335d90dcc163b61f56
mercklyzer <mercklyzer@gmail.com>
2023-01-09 21:35:16 +0800

    Attempt to fix video background on iphone 13

--- src/App.jsx
+++ src/App.jsx
@@ -30,38 +30,7 @@ function App() {
     return (
         <>
             <div className='bg-cream h-screen w-screen'>
-                <TransformWrapper
-                    initialScale={1.4}
-                    maxScale={3}
-                    centerOnInit
-                    wheel={{ step: 0.2 }}
-                    panning={{ excluded: ['panningDisabled'] }}
-                    pinch={{ excluded: ['pinchDisabled'], step:3 }}
-                >
-                    {({ zoomIn, zoomOut, resetTransform, ...rest }) => (
-                        <>
-                            <TransformComponent contentStyle={{width: '100%', height: '100%'}} wrapperStyle={{width:'100%', height:'100%'}}>
-
-                                <div className='h-full w-full relative'>
-                                    <video ref={mapRef} className="absolute top-0 left-0 -z-10 w-full h-full" autoPlay={true} muted={true} loop={true} playsInline={true}>
-                                        <source src={require('./assets/videos/mapp_resize_1.mp4')} type="video/mp4" />
-                                    </video>
-                                    <MapElements setShowLeftSideBar={setShowLeftSideBar}/>
-                                </div>
-                            </TransformComponent>
-
-                            <div className="fixed right-10 top-10 space-x-2 flex z-50">
-                                <VolumeButton />
-                                <MenuButton onMenu={() => setShowRightSideBar(true)} />
-                            </div>
-
-                            <div className='fixed right-10 top-0 bottom-0 h-full flex flex-col justify-center items-center'>
-                                <ZoomInButton onZoomIn={zoomIn} />
-                                <ZoomOutButton onZoomOut={zoomOut} />
-                            </div>
-                        </>
-                    )}
-                </TransformWrapper>
+                <Map setShowLeftSideBar={setShowLeftSideBar} setShowRightSideBar={setShowRightSideBar}/>
             </div>
 
             <SideBar showSideBar={showRightSideBar} setShowSideBar={setShowRightSideBar} position="right">
