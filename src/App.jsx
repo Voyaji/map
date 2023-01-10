9b1d1bad6a0f302e5eb879deb2b1a667eb6581a0
mercklyzer <mercklyzer@gmail.com>
2023-01-10 10:46:54 +0800

    Attempt to 100vh ios issue 1

--- src/App.jsx
+++ src/App.jsx
@@ -26,6 +26,17 @@ function App() {
         }, [1000])
     }, [])
 
+    useEffect(() => {
+        const appHeight = () => {
+            const doc = document.documentElement
+            doc.style.setProperty('--app-height', `${window.innerHeight}px`)
+        }
+        window.addEventListener('resize', appHeight)
+        appHeight()
+
+        return () => window.removeEventListener('resize', appHeight)
+    }, [])
+
     return (
         <>
             <div className='bg-cream h-screen w-screen'>
