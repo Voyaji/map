a4803cbd0a36a1aa28e532e990567abb34771ab8
mercklyzer <mercklyzer@gmail.com>
2023-01-23 16:47:14 +0800

    Update Background MP4

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -48,7 +48,7 @@ const Map = ({ setShowLeftSideBar, setShowRightSideBar, setLeftSideBarData, setS
                     <TransformComponent wrapperStyle={{ width: '100%', height: '100%' }}>
                         <div className='relative'>                            
                             <video ref={mapRef} preload='auto' className="relative z-10" autoPlay={true} muted={true} loop={true} playsInline={true} onLoadedData={() => setShowElements(true)}>
-                                <source src={require('../assets/videos/Map for animation2.mp4')} type="video/mp4" />
+                                <source src={require('../assets/videos/map looped.mp4')} type="video/mp4" />
                             </video>
                             {showElements && <MapElements setShowLeftSideBar={setShowLeftSideBar} setLeftSideBarData={setLeftSideBarData} setShowComingSoon={setShowComingSoon}/>}
                         </div>
@@ -66,39 +66,6 @@ const Map = ({ setShowLeftSideBar, setShowRightSideBar, setLeftSideBarData, setS
                 </>
             )}
         </TransformWrapper> : <></>
-
-    //     <TransformWrapper
-    //     initialScale={1.4}
-    //     maxScale={3}
-    //     centerOnInit
-    //     wheel={{ step: 0.2 }}
-    //     panning={{ excluded: ['panningDisabled'] }}
-    //     pinch={{ excluded: ['pinchDisabled'], step: 3 }}
-    // >
-    //     {({ zoomIn, zoomOut, resetTransform, ...rest }) => (
-    //         <>
-    //             <TransformComponent contentStyle={{ width: '100%', height: '100%' }} wrapperStyle={{ width: '100%', height: '100%' }}>
-
-    //                 <div className='h-full w-full relative'>
-    //                     <video preload={true} ref={mapRef} className="absolute top-0 left-0 -z-10 w-full h-full" autoPlay={true} muted={true} loop={true} playsInline={true} webkit-playsinline={true}>
-    //                         <source src={require('../assets/videos/mapp_resize_1.mp4')} type="video/mp4" />
-    //                     </video>
-    //                     <MapElements setShowLeftSideBar={setShowLeftSideBar} />
-    //                 </div>
-    //             </TransformComponent>
-
-    //             <div className="fixed right-10 top-10 space-x-2 flex z-50">
-    //                 <VolumeButton />
-    //                 <MenuButton onMenu={() => setShowRightSideBar(true)} />
-    //             </div>
-
-    //             <div className='fixed right-10 top-0 bottom-0 h-full flex flex-col justify-center items-center'>
-    //                 <ZoomInButton onZoomIn={zoomIn} />
-    //                 <ZoomOutButton onZoomOut={zoomOut} />
-    //             </div>
-    //         </>
-    //     )}
-    // </TransformWrapper>
     )
 }
 
